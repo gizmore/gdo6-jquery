8aa20e535a8cb266c2857e18095344e216aeb364
gizmore <gizmore@wechall.net>
2017-10-15 06:22:28 +0200

    6.05_fixes

--- Module_JQuery.php
+++ Module_JQuery.php
@@ -4,7 +4,7 @@ use GDO\Core\GDO_Module;
 use GDO\Core\Module_Core;
 final class Module_JQuery extends GDO_Module
 {
-    public $module_priority = 20;
+    public $module_priority = 5;
     
     public function onIncludeScripts()
     {
