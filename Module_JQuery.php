bd5bda5b377f8a4395b3692be549ddd49c571fc8
gizmore <gizmore@wechall.net>
2018-08-15 23:40:11 +0200

    6.08-r7-code-formatting

--- Module_JQuery.php
+++ Module_JQuery.php
@@ -4,16 +4,16 @@ use GDO\Core\GDO_Module;
 use GDO\Core\Module_Core;
 final class Module_JQuery extends GDO_Module
 {
-    public $module_priority = 5;
-    
-    public function onIncludeScripts()
-    {
-        $min = Module_Core::instance()->cfgMinifyJS() === 'no' ? '' : '.min';
-        
-        $this->addBowerJavascript("jquery/dist/jquery$min.js");
-        $this->addBowerJavascript("jquery-modal/jquery.modal$min.js");
-        $this->addBowerCSS("jquery-modal/jquery.modal$min.css");
+	public $module_priority = 5;
+	
+	public function onIncludeScripts()
+	{
+		$min = Module_Core::instance()->cfgMinifyJS() === 'no' ? '' : '.min';
+		
+		$this->addBowerJavascript("jquery/dist/jquery$min.js");
+		$this->addBowerJavascript("jquery-modal/jquery.modal$min.js");
+		$this->addBowerCSS("jquery-modal/jquery.modal$min.css");
 
-        $this->addJavascript('js/gdo-jquery.js');
-    }
+		$this->addJavascript('js/gdo-jquery.js');
+	}
 }
