4fac23977075e41503d4a1bcb628cc220866f5ab
gizmore <gizmore@wechall.net>
2021-05-06 01:23:16 +0200

    6.10.2-r1071

--- js/gdo-jquery.js
+++ js/gdo-jquery.js
@@ -1,9 +1,8 @@
 "use strict"
 window.GDO.error = function(html, title) {
-	console.error(html);
 	var message = html.error ? html.error : '';
-	var message = message ? message : (html.data ? html.data.error : '');
-	var dialog = $('<div id="gdo-error-dialog">'+message+'</div>');
+	var message = message ? message : (html.json ? html.json.error : '');
+	var dialog = $('<div id="gdo-error-dialog">' + message + '</div>');
 	$(window.document.body).append(dialog);
 	$('#gdo-error-dialog').on($.modal.BEFORE_CLOSE, function(event, modal) {
 		dialog.remove();
