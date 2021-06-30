3d409d433d9cbb0241a8f5c1a53f192840de5da0
gizmore <gizmore@wechall.net>
2021-06-30 04:08:09 +0200

    6.10.4-r1172

--- js/gdo-jquery.js
+++ js/gdo-jquery.js
@@ -1,10 +1,13 @@
 "use strict";
+
 window.GDO.error = function(html, title) {
-	console.log(html);
-	var dialog = $('<div id="gdo-error-dialog">' + html + '</div>');
-	$('#gdo-pagewrap').append(dialog);
+	console.error(html);
+	var dialog = $('<div id="gdo-error-dialog" class="modal">' + html + '</div>');
+	$('body').append(dialog);
 	$('#gdo-error-dialog').on($.modal.BEFORE_CLOSE, function(event, modal) {
 		dialog.remove();
 	});
-	$('#gdo-error-dialog').modal();
+	$('#gdo-error-dialog').modal({
+		closeClass: 'icon-remove',
+	});
 };
