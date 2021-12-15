4e31da51e51b12669181edd3aed48ec45cd1e4c3
gizmore <gizmore@wechall.net>
2021-12-15 01:10:35 +0100

    6.11.1-r6258

--- Module_JQuery.php
+++ Module_JQuery.php
@@ -6,9 +6,10 @@ use GDO\Javascript\Module_Javascript;
 
 /**
  * This module adds jquery to your application.
+ * @TODO: Optional script included: jquery-color.
  * 
  * @author gizmore
- * @version 6.10.3
+ * @version 6.11.1
  * @since 6.0.0
  */
 final class Module_JQuery extends GDO_Module
@@ -17,7 +18,7 @@ final class Module_JQuery extends GDO_Module
 	
 	public function onIncludeScripts()
 	{
-		$min = Module_Javascript::instance()->jsMinAppend();
+		$min = Module_Javascript::instance()->cfgMinAppend();
 		
 		$this->addBowerJS("jquery/dist/jquery$min.js");
 		$this->addBowerJS("jquery-modal/jquery.modal$min.js");
@@ -25,6 +26,8 @@ final class Module_JQuery extends GDO_Module
 
 		$this->addCSS('css/gdo-jquery.css');
 		$this->addJS('js/gdo-jquery.js');
+		
+		$this->addBowerJS("jquery-color/dist/jquery.color{$min}.js");
 	}
 	
 	public function getModuleLicenseFilenames()
