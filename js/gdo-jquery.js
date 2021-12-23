7b1bd7dbbae8faaf45e1945391c2928bedc9d1f5
gizmore <gizmore@wechall.net>
2021-12-23 11:51:10 +0100

    6.11.1-r6270

--- js/gdo-jquery.js
+++ js/gdo-jquery.js
@@ -1,18 +1,28 @@
 "use strict";
-
-window.GDO.error = function(html, title) {
-	console.error(html);
-	var dialog = $('<div id="gdo-error-dialog" class="modal">' + html + '</div>');
-	$('body').append(dialog);
-	dialog.modal({
-		closeClass: 'icon-remove',
-	});
-	dialog.on($.modal.CLOSE, function(event, modal) {
-		dialog.remove();
-	});
-};
-
 $(function(){
+
+	window.GDO.shortDebugURL = function(url) {
+		let pattern = '(' + GDO_PROTOCOL + "://" + GDO_DOMAIN + GDO_WEB_ROOT;
+		pattern += '([^? ]+)[ ?$][^ ]*)';
+		pattern = new RegExp(pattern);
+		return url.replace(pattern, '<a href="$1">$2</a>');
+	};
+
+	window.GDO.error = function(html, title) {
+//		console.error(html);
+		var dialog = $('<div id="gdo-error-dialog" class="modal">' + html + '</div>');
+		$('body').append(dialog);
+		dialog.modal({
+			closeClass: 'icon-remove',
+		});
+		let defer = $.Deferred();
+		dialog.on($.modal.CLOSE, function() {
+			dialog.remove();
+			defer.resolve();
+		});
+		return defer.promise();
+	};
+
 	$.ajaxSetup({
 		headers: {
 			'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
